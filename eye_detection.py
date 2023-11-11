092f15ceb06141437cfde1fe8f58855e59b82f35
Muhammad Tayyab Tahir Qureshi <m.tayyabtq@gmail.com>
2023-11-11 14:42:10 +0500

    adding functionality for live vid stream

--- eye_detection.py
+++ eye_detection.py
@@ -30,5 +30,39 @@ for (x,y,w,h) in face_rect:
     cv.rectangle(img, (x,y), (x+w,y+h), (255,0,0), thickness=2)
 
 cv.imshow('Detected face and eyes', img)
+
+
 cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+
+
+
+capture = cv.VideoCapture(0)
+capture.set(3, 500)
+capture.set(4, 700)
+capture.set(10, 90)
+
+while True:
+    success, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    eyes_rect = haar_cascade_eye.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+    face_rect = haar_cascade_face.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2)
+
+    for (x,y,w,h) in eyes_rect:
+        cv.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+    
+    for (x,y,w,h) in face_rect:
+        cv.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), thickness=2)
+
+    cv.imshow('Detected Face and Eyes', frame)
+
+    if cv.waitKey(1) & 0xFF == ord('d'):
+        break
+
+capture.release()
 cv.destroyAllWindows()
